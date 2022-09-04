c01e7155ccafc3c57b1481b7675804b2e39e5fae
Asif_Ahamed <mdasifahamedfahim@gmail.com>
2022-09-04 21:45:54 +0600

    Converted to Function with user define array size and user define numbers to summed.

--- sum_of_an_array.c
+++ sum_of_an_array.c
@@ -1,19 +1,36 @@
 #include<stdio.h>
+int sum_of_array(int arr[], int n){
+
+    int i, a, sum, b =0;
+
+    for(i = 0; i<n; i++){
+
+        scanf("%d", &arr[i]);
+    }
+    for(i = 0; i<n; i++){
+
+        a = arr[i];
+        sum = a + b;
+        b = sum;
+    }
+    return sum;
+}
+
 int main(){
 
 
-    int arr[] = {10,20,30,40,50,60,70,80};
-    int a,i,sum,b=0;
 
-    for(i=0; i<8; i++){
+    int sum;
+    int n;
+    printf("Please Input How Many Number Need To Be Summed : ");
+    scanf("%d", &n);// At first size of the Array Need to be determined
+    int arr[n]; // Then the array should be initiated.
+    printf("Now Enter The Numbers and Press Enter After Input One By One : \n");
 
-        a = arr[i];
-        sum = a+ b;
-        b = sum;
-    }
-    printf("The sum of the Arrray is : %d  ", sum);
+    sum = sum_of_array(arr, n);
+
+    printf("The Sum of  All The Numbers is : %d", sum);
 
     return 0;
-
-
 }
+
