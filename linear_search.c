4bfb94f3cf4364c042102f5c52b8f6c1557738fb
Asif_Ahamed <mdasifahamedfahim@gmail.com>
2022-09-05 11:38:45 +0600

    Converted to function

--- linear_search.c
+++ linear_search.c
@@ -1,22 +1,58 @@
 #include<stdio.h>
-int main(){
-
-    int arr[] = {10,20,30,40,5,78,98,55};
-    int data = 5;
-    int found =0;// before starting we have set found 0 that no is still founded.
+int Linear_search(int arr[], int size, int data){
 
 
-    for(int i =0; i<8; ){
-        if(data == arr[i]){
-            printf("Data Is Found and Its Index is : %d  ", i);
-            found =1; // this is the break point if data this found will be changed to 1 if data is not found then the search will be continued.
+    int found = 0, i; // before starting we have set found 0 that no is still founded.
+
+
+    for(i=0; i<size;){
+
+        if(arr[i]==data){
+
+            found = 1; // this is the break point if data this found will be changed to 1 if data is not found then the search will be continued.
             break;
-        }else {
+        }
+        else{
+
             i++;
         }
     }
-    if(found==0){ // this found means the is not never found on the loop thats why it is still 0;
-        printf("Data Is Not Found!");
+    if(found==0){ // this found 0 means the data is  never found on the loop thats why it is still 0;
+
+        i = -1; // this i = -1 will work in the main to determine that the data is found or not.
+        return i;
+    }else if(found == 1){
+
+        return i;
+    }
+}
+
+int main(){
+
+    int size;
+    int i;
+    int data;
+    printf("Enter The Size of The Array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+    printf("Enter Array Element : \n");
+    for(int i=0; i<size; i++){
+
+        scanf("%d", &arr[i]);
+
+    }
+    printf("Enter The Data To Find : ");
+
+    scanf("%d", &data);
+
+    i = Linear_search(arr,size,data);
+    if(i==-1){
+        printf("Data is not Found!");
+    }
+    else{
+
+        printf("The Element Is in the Index : %d", i);
     }
 
     return 0;
